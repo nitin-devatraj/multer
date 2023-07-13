ff99b8f3e73e592bb000313d8e31c1a6f4d5a8f6
nitin devatraj <nitindevatraj@protonmail.com>
2023-07-13 13:54:30 +0530

    create AppointmentForm component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import styles from "./App.module.css";
+import AppointmentForm from "./components/appointment-form/AppointmentForm";
 import BenefitSection from "./components/benefits-section/BenefitSection";
 import Header from "./components/header/Header";
 
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <section className={styles.wrapper}>
         <BenefitSection />
+        <AppointmentForm />
       </section>
     </>
   );
---
+++ src/components/appointment-form/AppointmentForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./AppointmentForm.module.css";
+
+function AppointmentForm() {
+  return (
+    <form className={styles.form}>
+      <div className={styles.cta_wrapper}>
+        <h3>
+          Schedule an <br /> Appointment
+        </h3>
+        <p>
+          Here, let visitors know what will happen <br /> when they complete
+          your form.
+        </p>
+      </div>
+      <div className={styles.input_wrapper}>
+        <input type="text" name="fname" placeholder="First name*" />
+        <input type="text" name="lname" placeholder="Last name*" />
+        <input type="email" name="email" placeholder="Email*" />
+        <input type="tel" name="phnumber" placeholder="Phone Number*" />
+        <select name="type">
+          <option value="type" selected>
+            Type Of Appointment
+          </option>
+          <option value="type1">Type 1</option>
+          <option value="type2">Type 2</option>
+          <option value="type3">Type 3</option>
+        </select>
+        <button>SCHEDULE NOW</button>
+      </div>
+    </form>
+  );
+}
+
+export default AppointmentForm;
